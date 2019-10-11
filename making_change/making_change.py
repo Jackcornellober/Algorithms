50a86a2f76144519c212e10d343b1e987e066b69
jackcornellober <jackcornellober@gmail.com>
2019-10-11 01:22:33 -0700

    mvp done... wow... stupid bug literally took hours to find

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,28 +2,53 @@
 
 import sys
 
+# def making_change(amount, denominations):
+#   total = 0
+
+#   if len(denominations) == 1:
+#     return 1
+#   else:
+#     if (amount - (denominations[-1])) >= 0:
+#       q = amount//denominations
+#       b = q*making_change(amount - denominations[-1]*q,denominations)
+#       popped = denominations
+#       popped.pop()
+#       return b + making_change(amount-denominations[-1]*q,popped)
+#     else:
+#       popped = denominations
+#       popped.pop()
+# #       return making_change(amount,popped)
+
+
+
+#   for i in denominations:
+#     if i >= amount:
+#       total = total + making_change
+
 def making_change(amount, denominations):
-  total = 0
+  if amount == 0:
+    return 1
 
   if len(denominations) == 1:
     return 1
+
+
+
   else:
     if (amount - (denominations[-1])) >= 0:
-      q = amount//denominations
-      b = q*making_change(amount - denominations[-1]*q,denominations)
-      popped = denominations
-      popped.pop()
-      return b + making_change(amount-denominations[-1]*q,popped)
+      q = amount//denominations[-1]
+      b = 0 
+      popped = list(denominations) 
+      z = popped.pop()
+      for i in range(0,q+1):
+        b = b + making_change(amount - z*i,popped)
+      
+
+      return b
     else:
       popped = denominations
       popped.pop()
       return making_change(amount,popped)
-
-
-
-  for i in denominations:
-    if i >= amount:
-      total = total + making_change
 
 
 if __name__ == "__main__":
